8bb5df6114acd9cc94db818e0118b1b8e9e6c4f2
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-27 20:48:02 +0100

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useState, React } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-    let name = document.getElementById("name")
+    const initialValues = {username: "", email: "", password: ""}
+    const [formValues, setFormValues] = useState(initialValues)
 
     let handleform = () => {
         // nameValue = name.value
@@ -20,11 +21,26 @@ const Login = () => {
         <p className='text-inactive mt-3'>Enter details to login</p>
             <form method='POST' action="" className='flex flex-col max-w-[600px] w-full mx-auto my-5'>
                 <label className="text-left my-2">Full Name</label>
-                <input className='bg-[#ccd6f6] p-2' type="text" id="name" placeholder='Full Name' name='name' />
+                <input 
+                    className='bg-[#ccd6f6] p-2' 
+                    type="text" id="name" 
+                    placeholder='Full Name' 
+                    name='name' 
+                    value={ formValues.username } />
                 <label className="text-left my-2">E-mail</label>
-                <input className='mb-2 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+                <input 
+                    className='mb-2 p-2 bg-[#ccd6f6]' 
+                    type="email" 
+                    placeholder='Email' 
+                    name='email'
+                    value={ formValues.email } />
                 <label className="text-left my-2">Password</label>
-                <input className='mb-2 p-2 bg-[#ccd6f6]' type="password" placeholder='password' name='email' />
+                <input 
+                    className='mb-2 p-2 bg-[#ccd6f6]' 
+                    type="password" 
+                    placeholder='password' 
+                    name='email'
+                    value={ formValues.password } />
                 <Link to='/profile' onClick={handleform} className='mt-3'>
                     <button  className='border-2 border-black hover:bg-gray-700 hover:text-white hover:border-gray-600 py-2 px-4 rounded-lg my-8 mx-auto flex items-center'>Login</button>
                 </Link>
