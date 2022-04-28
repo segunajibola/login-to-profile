e206ee6323b6ebd23adb4c1a70f97324085ae8f6
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-28 08:54:44 +0100

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
-import { useState, React } from 'react';
+import { useState, useEffect, React } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
 
     const initialValues = {username: "", email: "", password: ""}
     const [formValues, setFormValues] = useState(initialValues)
+    const [formErrors, setFormErrors] = useState({})
+    const [isSubmit, setIsSubmit] = useState(false)
 
     const handleChange = (e) => {
         // console.log(e.target);
@@ -15,26 +17,63 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFormErrors(validate(formValues))
+        setIsSubmit(true)
     } 
-    
 
+    useEffect(() => {
+        console.log(formErrors)
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(formValues)
+        }
+    }, [formErrors]);
+
+    const validate = (values) => {
+        const errors = {}
+        // /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        // /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
+        // ^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$
+        if (!values.username) {
+            errors.username = "Username is required!"
+        }
+        if (!values.email) {
+            errors.email = "Email is required!"
+        } else if (!regex.test(values.email)) {
+            errors.email = "This is not a valid email format!";
+        }
+        if (!values.password) {
+            errors.password = "Password is required!"
+        } else if (values.password.length < 4) {
+            errors.password = "Password must be more than 4 characters.";
+        } else if (values.password.length > 10) {
+            errors.password = "Password must be not be more than 10 characters.";
+        }
+        return errors;
+    }
+    
     return (
     <>
       <div className='w-full pt-24 mx-auto px-6 text-center'>
         <h1 className='text-primary mt-8 md:mt-0 font-bold text-3xl'>
           Welcome to E-Gift
         </h1>
-        <pre>{JSON.stringyfy(formValues(formValues, undefined, 2))}</pre>
+        {Object.keys(formErrors).length === 0 && isSubmit ? (
+            <div>Signed in successfully</div>) : (
+                <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+            )}
         <p className='text-inactive mt-3'>Enter details to login</p>
             <form method='POST' action="" onSubmit={handleSubmit} className='flex flex-col max-w-[600px] w-full mx-auto my-5'>
                 <label className="text-left my-2">Full Name</label>
                 <input 
                     className='bg-[#ccd6f6] p-2' 
-                    type="text" id="fullname" 
+                    type="text"
+                    id="fullname" 
                     placeholder='Full Name' 
                     name='username' 
                     value={ formValues.username }
                     onChange={handleChange} />
+                <p>{ formErrors.username }</p>
                 <label className="text-left my-2">E-mail</label>
                 <input 
                     className='mb-2 p-2 bg-[#ccd6f6]' 
@@ -43,6 +82,7 @@ const Login = () => {
                     name='email'
                     value={ formValues.email }
                     onChange={handleChange} />
+                <p>{ formErrors.email }</p>
                 <label className="text-left my-2">Password</label>
                 <input 
                     className='mb-2 p-2 bg-[#ccd6f6]' 
@@ -51,9 +91,10 @@ const Login = () => {
                     name='password'
                     value={ formValues.password }
                     onChange={handleChange} />
-                <Link to='/profile' className='mt-3'>
-                    <submit  className='border-2 w-20 border-black hover:bg-gray-700 hover:text-white hover:border-gray-600 py-2 px-4 rounded-lg my-8 mx-auto flex items-center'>Login</submit>
-                </Link>
+                <p>{ formErrors.password }</p>
+                {/* <Link to='/profile' className='mt-3'> */}
+                    <button className='border-2 w-20 border-black hover:bg-gray-700 hover:text-white hover:border-gray-600 py-2 px-4 rounded-lg my-8 mx-auto flex items-center'>Login</button>
+                {/* </Link> */}
             </form>
           <Link to='/' className='mt-3 text-indigo-600 text-sm'>
             Forgot Password?
